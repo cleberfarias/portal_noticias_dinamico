416b140fb414861c3c99972526d5b53fc949027e
cleberfarias <cleber_afd@hotmail.com>
2025-01-10 11:44:32 -0300

    .

--- Posts.js
+++ Posts.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const postSchema = new Schema({
-    titulo: { type: String, required: true },
-    imagem: { type: String, required: true },
+    titulo: { type: String,},
+    imagem: { type: String,  },
     categoria: { type: String },
-    conteudo: { type: String, required: true },
+    conteudo: { type: String,  },
     slug: { type: String, required: true, unique: true },
     autor: { type: String },
     views: { type: Number, default: 0 }
